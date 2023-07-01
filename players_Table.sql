765ca597ee9e058b163781478f20804d0d80c999
jules onguene messolo <lamessgdbless@gmail.com>
2023-07-01 15:38:53 +0200

    Analyse with MySQL and python

--- players_Table.sql
+++ players_Table.sql
@@ -1,7 +1,7 @@
-use mysql_learners;
+use goldplayersdatabase;
 /*
-DROP TABLE IF EXISTS players;
-CREATE TABLE players (
+DROP TABLE IF EXISTS players90s;
+CREATE TABLE players90s (
 id INT PRIMARY KEY,
 Rang INT NOT NULL,
 Nom VARCHAR(50),
@@ -9,21 +9,32 @@ Club VARCHAR(50),
 Selection VARCHAR(50),
 Poste VARCHAR(50),
 Points VARCHAR(50),
+Confederation VARCHAR(50),
+Type VARCHAR (50),
 Pourcentagevote VARCHAR(50),
 Annee  VARCHAR(50)
 );
 */
-SELECT * FROM players WHERE Rang = 1;
-SELECT  Poste, SUM(Points) FROM Players GROUP BY Poste;
-SELECT Selection,  COUNT(Selection) FROM players GROUP BY Selection ;
+
+SELECT * FROM players90s WHERE Rang = 1;
+SELECT  Poste, SUM(Points) FROM Players90s GROUP BY Poste;
+SELECT Selection,  COUNT(Selection) FROM players90s GROUP BY Selection ;
 
 /* nous calculons la somme totale des points  pour chaque joueur et le MAX de point atteint par un joueur  */
-SELECT * FROM PLAYERS;
+SELECT * FROM PLAYERS90s;
 
 SELECT  Nom, Selection, Points, 
 SUM(Points) OVER (PARTITION BY Nom)  AS Total_pts,
 MAX(Points) OVER (PARTITION BY Rang)  AS Max_pts
-FROM Players;
+FROM Players90s;
 
-SELECT Nom,Selection, SUM(Points) AS Sum_pts,Club FROM PLAYERS
+SELECT Nom,Selection, SUM(Points) AS Sum_pts, COUNT(Nom),Club FROM PLAYERS
 GROUP BY Nom,Selection HAVING Count(*) >= 1 ORDER BY Sum_pts DESC;
+
+/* Le club qui a le plus de representant pour la table 2016_22 */
+SELECT Club, COUNT(*)  FROM players GROUP BY Club;
+
+/* Le club qui a le plus de representant pour la table 90_99 */
+SELECT Club, COUNT(*)  FROM players90s GROUP BY Club;
+
+
